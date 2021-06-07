acbe15c04de36ce0b3398be19d4ef1410d775d34
Mr-Ojii <okaschan@gmail.com>
2021-06-07 23:31:39 +0900

    First Commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# getMicroseconds-Rust
+マイクロ秒単位の現在時間を64bitのunsigned intとして返すライブラリです。
+
+``cargo build --lib --release``
+
+などでコンパイルしてお使いください。
---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+#![allow(non_snake_case)]
+use std::time::{SystemTime, UNIX_EPOCH};
+
+#[no_mangle]
+pub extern "C" fn getNowMicroseconds() -> u64 {
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    let nanos = since_the_epoch.subsec_nanos() as u64;
+    let micros = (1000*1000*1000 * since_the_epoch.as_secs() + nanos)/(1000);
+    return micros;
+}
+
+#[test]
+fn it_works() {
+    for i in 0..100 {
+        println!("{} microsec", getNowMicroseconds());
+    }
+}
