7086f666498d3d510643ec9aa59917a2bc00bfb3
Mr-Ojii <okaschan@gmail.com>
2023-05-18 21:22:27 +0900

    exceptをunwrap_orに変更

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 #![allow(non_snake_case)]
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 #[no_mangle]
 pub extern "C" fn getNowMicroseconds() -> u64 {
     let start = SystemTime::now();
     let since_the_epoch = start
         .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards");
+        .unwrap_or(Duration::default());
     let nanos = since_the_epoch.subsec_nanos() as u64;
     let micros = (1000*1000*1000 * since_the_epoch.as_secs() + nanos)/(1000);
     return micros;
@@ -15,6 +15,6 @@ pub extern "C" fn getNowMicroseconds() -> u64 {
 #[test]
 fn it_works() {
     for i in 0..100 {
-        println!("{} microsec", getNowMicroseconds());
+        println!("{} microsec({})", getNowMicroseconds(), i);
     }
 }
